6bf5971269360d6002ffd1803f6ebf9f3a451fd5
rayhankinan <rayhankinan@gmail.com>
2024-02-28 04:01:51 +0700

    Add torch and torchvision

--- src/torchseal/example.py
+++ src/torchseal/example.py
@@ -1,5 +1,15 @@
 import tenseal as ts
+import torch
+import torchvision
 
 
 def get_tenseal_version() -> str:
     return ts.__version__
+
+
+def get_torch_version() -> str:
+    return torch.__version__
+
+
+def get_torchvision_version() -> str:
+    return torchvision.__version__
