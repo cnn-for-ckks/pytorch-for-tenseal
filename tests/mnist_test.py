fed848fffc4bbda2c3687cc04f33977bcb5f659b
rayhankinan <rayhankinan@gmail.com>
2024-02-29 06:10:50 +0700

    split mnist script

--- tests/mnist_test.py
+++ tests/mnist_test.py
@@ -2,96 +2,12 @@ from typing import Tuple
 from torch.utils.data import DataLoader, RandomSampler
 from torchvision import datasets, transforms
 from tenseal import CKKSVector
+from mnist_train import ConvNet, test
 
 import numpy as np
 import torch
 import torchseal
 import tenseal as ts
-
-
-class ConvNet(torch.nn.Module):
-    def __init__(self, hidden=64, output=10):
-        super(ConvNet, self).__init__()
-        self.conv1 = torch.nn.Conv2d(1, 4, kernel_size=7, padding=0, stride=3)
-        self.fc1 = torch.nn.Linear(256, hidden)
-        self.fc2 = torch.nn.Linear(hidden, output)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.conv1(x)
-
-        # the model uses the square activation function
-        x = x * x
-
-        # flattening while keeping the batch axis
-        x = x.view(-1, 256)
-        x = self.fc1(x)
-        x = x * x
-        x = self.fc2(x)
-
-        return x
-
-
-def train(model: ConvNet, train_loader: DataLoader, criterion: torch.nn.CrossEntropyLoss, optimizer: torch.optim.Adam, n_epochs=10):
-    # model in training mode
-    model.train()
-    for epoch in range(1, n_epochs+1):
-
-        train_loss = 0.0
-        for data, target in train_loader:
-            optimizer.zero_grad()
-            output = model(data)
-            loss = criterion.forward(output, target)
-            loss.backward()
-            optimizer.step()
-            train_loss += loss.item()
-
-        # calculate average losses
-        train_loss = train_loss / len(train_loader)
-
-        print("Epoch: {} \tTraining Loss: {:.6f}".format(epoch, train_loss))
-
-    # model in evaluation mode
-    model.eval()
-    return model
-
-
-def test(model: ConvNet, test_loader: DataLoader, criterion: torch.nn.CrossEntropyLoss):
-    # initialize lists to monitor test loss and accuracy
-    test_loss = 0.0
-    class_correct = list(0. for _ in range(10))
-    class_total = list(0. for _ in range(10))
-
-    # model in evaluation mode
-    model.eval()
-
-    for data, target in test_loader:
-        output = model(data)
-        loss = criterion(output, target)
-        test_loss += loss.item()
-        # convert output probabilities to predicted class
-        _, pred = torch.max(output, 1)
-        # compare predictions to true label
-        correct = np.squeeze(pred.eq(target.data.view_as(pred)))
-        # calculate test accuracy for each object class
-        for i in range(len(target)):
-            label = target.data[i]
-            class_correct[label] += correct[i].item()
-            class_total[label] += 1
-
-    # calculate and print avg test loss
-    test_loss = test_loss/len(test_loader)
-    print(f"Test Loss: {test_loss:.6f}\n")
-
-    for label in range(10):
-        print(
-            f"Test Accuracy of {label}: {int(100 * class_correct[label] / class_total[label])}% "
-            f"({int(np.sum(class_correct[label]))}/{int(np.sum(class_total[label]))})"
-        )
-
-    print(
-        f"\nTest Accuracy (Overall): {int(100 * np.sum(class_correct) / np.sum(class_total))}% "
-        f"({int(np.sum(class_correct))}/{int(np.sum(class_total))})"
-    )
 
 
 class EncConvNet:
@@ -185,7 +101,7 @@ def enc_test(context: ts.Context, enc_model: EncConvNet, test_loader: DataLoader
 
     # calculate and print avg test loss
     test_loss = test_loss / sum(class_total)
-    print(f"Test Loss: {test_loss:.6f}\n")
+    print(f"Test Loss for Encrypted Data: {test_loss:.6f}\n")
 
     for label in range(10):
         print(
@@ -194,7 +110,7 @@ def enc_test(context: ts.Context, enc_model: EncConvNet, test_loader: DataLoader
         )
 
     print(
-        f"\nTest Accuracy (Overall): {int(100 * np.sum(class_correct) / np.sum(class_total))}% "
+        f"\nTest Accuracy for Encrypted Data (Overall): {int(100 * np.sum(class_correct) / np.sum(class_total))}% "
         f"({int(np.sum(class_correct))}/{int(np.sum(class_total))})"
     )
 
@@ -204,9 +120,6 @@ if __name__ == "__main__":
     torch.manual_seed(73)
 
     # Load the data
-    train_data = datasets.MNIST(
-        "data", train=True, download=True, transform=transforms.ToTensor()
-    )
     test_data = datasets.MNIST(
         "data", train=False, download=True, transform=transforms.ToTensor()
     )
@@ -215,26 +128,23 @@ if __name__ == "__main__":
     batch_size = 64
 
     # Create the samplers
-    sampler = RandomSampler(test_data, num_samples=50)
+    sampler = RandomSampler(test_data, num_samples=50, replacement=True)
 
     # Create the data loaders
-    train_loader = DataLoader(
-        train_data, batch_size=batch_size, shuffle=True
-    )
     test_loader = DataLoader(
         test_data, batch_size=batch_size, sampler=sampler
     )
 
-    # Create the model, criterion, and optimizer
+    # Load the model
     model = ConvNet()
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    model.load_state_dict(torch.load("./parameters/MNIST/model.pth"))
 
-    # Train the model
-    model = train(model, train_loader, criterion, optimizer, 10)
+    # Loss function
+    criterion = torch.nn.CrossEntropyLoss()
 
     # Test the model
     test(model, test_loader, criterion)
+    print()
 
     # Controls precision of the fractional part
     bits_scale = 26
@@ -269,3 +179,4 @@ if __name__ == "__main__":
         (7, 7),
         3
     )
+    print()
