Trying to fix backprop

src/torchseal/function/conv2d.py
@@ -25,7 +25,7 @@ class Conv2dFunction(Function):
 
     # NOTE: This method requires private key access
     @staticmethod
-    def backward(ctx: Conv2dFunctionCtx, enc_grad_output: CKKSVector) -> Tuple[Optional[Tensor], Optional[Tensor], Optional[Tensor]]:
+    def backward(ctx: Conv2dFunctionCtx, grad_output: Tensor) -> Tuple[Optional[Tensor], Optional[Tensor], Optional[Tensor]]:
         # Get the saved tensors
         saved_tensors: Tuple[Tensor] = ctx.saved_tensors  # type: ignore
         enc_x = ctx.enc_x
@@ -38,10 +38,6 @@ class Conv2dFunction(Function):
 
         # Decrypt the encrypted input and gradient
         x = torch.tensor(enc_x.decrypt(), requires_grad=True)
-        grad_output = torch.tensor(
-            enc_grad_output.decrypt(),
-            requires_grad=True
-        )
 
         # Initialize the gradients
         grad_input = grad_weight = grad_bias = None
@@ -49,11 +45,3 @@ class Conv2dFunction(Function):
         # TODO: Compute the gradients
 
         return grad_input, grad_weight, grad_bias
-
-    @staticmethod
-    def apply(enc_x: CKKSVector, weight: Tensor, bias: Tensor, windows_nb: int) -> CKKSVector:
-        result: CKKSVector = super(Conv2dFunction, Conv2dFunction).apply(
-            enc_x, weight, bias, windows_nb
-        )  # type: ignore
-
-        return result

src/torchseal/function/linear.py
@@ -22,7 +22,7 @@ class LinearFunction(Function):
 
     # NOTE: This method requires private key access
     @staticmethod
-    def backward(ctx: LinearFunctionCtx, enc_grad_output: CKKSVector) -> Tuple[Optional[Tensor], Optional[Tensor], Optional[Tensor]]:
+    def backward(ctx: LinearFunctionCtx, grad_output: Tensor) -> Tuple[Optional[Tensor], Optional[Tensor], Optional[Tensor]]:
         # Get the saved tensors
         saved_tensors: Tuple[Tensor] = ctx.saved_tensors  # type: ignore
         enc_x = ctx.enc_x
@@ -35,10 +35,6 @@ class LinearFunction(Function):
 
         # Decrypt the encrypted input and gradient
         x = torch.tensor(enc_x.decrypt(), requires_grad=True)
-        grad_output = torch.tensor(
-            enc_grad_output.decrypt(),
-            requires_grad=True
-        )
 
         # Initialize the gradients
         grad_input = grad_weight = grad_bias = None
@@ -52,11 +48,3 @@ class LinearFunction(Function):
             grad_bias = grad_output.sum(0)
 
         return grad_input, grad_weight, grad_bias
-
-    @staticmethod
-    def apply(enc_x: CKKSVector, weight: Tensor, bias: Tensor) -> CKKSVector:
-        result: CKKSVector = super(LinearFunction, LinearFunction).apply(
-            enc_x, weight, bias
-        )  # type: ignore
-
-        return result

src/torchseal/function/sigmoid.py
@@ -28,28 +28,20 @@ class SigmoidFunction(Function):
         return result
 
     @staticmethod
-    def backward(ctx: SigmoidFunctionCtx, enc_grad_output: CKKSVector) -> Tuple[Tensor]:
+    def backward(ctx: SigmoidFunctionCtx, grad_output: Tensor) -> Tuple[Tensor]:
         # Get the saved tensors
         enc_x = ctx.enc_x
 
         # Decrypt the encrypted input and gradient
-        x = torch.tensor(enc_x.decrypt(), requires_grad=True)
-        grad_output = torch.tensor(
-            enc_grad_output.decrypt(), requires_grad=True
+        out_x = torch.tensor(
+            list(map(lambda x: 0.197 - 0.008 * x, enc_x.decrypt())),
+            requires_grad=True
         )
 
         # TODO: Do approximation of the sigmoid function using the polynomial approximation
 
         # Compute the gradients
         # NOTE: This is just an example
-        grad_input = grad_output.mm(x.apply_(lambda x: 0.197 - 0.008 * x))
+        grad_input = grad_output.mm(out_x)
 
         return grad_input,
-
-    @staticmethod
-    def apply(enc_x: CKKSVector) -> CKKSVector:
-        result: CKKSVector = super(
-            SigmoidFunction, SigmoidFunction
-        ).apply(enc_x)  # type: ignore
-
-        return result

src/torchseal/function/square.py
@@ -23,25 +23,16 @@ class SquareFunction(Function):
         return result
 
     @staticmethod
-    def backward(ctx: SquareFunctionCtx, enc_grad_output: CKKSVector) -> Tuple[Tensor]:
+    def backward(ctx: SquareFunctionCtx, grad_output: Tensor) -> Tuple[Tensor]:
         # Get the saved tensors
         enc_x = ctx.enc_x
 
         # Decrypt the encrypted input and gradient
-        x = torch.tensor(enc_x.decrypt(), requires_grad=True)
-        grad_output = torch.tensor(
-            enc_grad_output.decrypt(), requires_grad=True
+        x = torch.tensor(
+            list(map(lambda x: 2 * x, enc_x.decrypt())), requires_grad=True
         )
 
         # Compute the gradients
-        grad_input = grad_output.mm(x.apply_(lambda x: 2 * x))
+        grad_input = grad_output.mm(x)
 
         return grad_input,
-
-    @staticmethod
-    def apply(enc_x: CKKSVector) -> CKKSVector:
-        result: CKKSVector = super(
-            SquareFunction, SquareFunction
-        ).apply(enc_x)  # type: ignore
-
-        return result