5679d34e4ef1efdab02768cc90dc7dc48a1e53e4
rayhankinan <rayhankinan@gmail.com>
2024-03-06 16:59:18 +0700

    Add several notes

--- tests/TenSEAL/im2col_encoding.py
+++ tests/TenSEAL/im2col_encoding.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
 
     # Randomize the tensor
     weight = torch.tensor(
-        [[[0.1, 0.2, 0.3, 0.4, 0.5, 0.6] for _ in range(6)]]
+        [[[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8] for _ in range(8)]]
     )
 
     # NOTE: Plaintext im2col
@@ -36,8 +36,9 @@ if __name__ == "__main__":
         ]]
     )
     print("image.shape:", image.shape, end="\n\n")
+    print(image, end="\n\n")
 
-    unfolded_image = unfold(image, kernel_size=(6, 6), stride=3)
+    unfolded_image = unfold(image, kernel_size=(8, 8), stride=3)
     print("unfolded_image.shape:", unfolded_image.shape, end="\n\n")
 
     # NOTE: Plaintext convolution
@@ -71,21 +72,16 @@ if __name__ == "__main__":
     # Decrypt the result
     print("enc_result:", enc_result.decrypt(), end="\n\n")
 
-    # NOTE: Plaintext col2im
+    # NOTE: Plaintext col2im (Currently only works when kernel size is power of 2)
+    # NOTE: Also still broken
     raw_dec_unfolded_image = enc_unfolded_image.decrypt()
-    dec_unfolded_image = torch.tensor(enc_unfolded_image.decrypt()).reshape(
+    dec_unfolded_image = torch.tensor(raw_dec_unfolded_image).reshape(
         len(raw_dec_unfolded_image) // num_col, num_col
     )
     print("dec_unfolded_image.shape", dec_unfolded_image.shape, end="\n\n")
 
-    # Throw away padding
-    dec_unfolded_image_cleaned = dec_unfolded_image[:num_row, :]
-    print(
-        "dec_unfolded_image_cleaned.shape:",
-        dec_unfolded_image_cleaned.shape, end="\n\n"
-    )
-
     dec_image = fold(
-        dec_unfolded_image_cleaned, output_size=(28, 28), kernel_size=(6, 6), stride=3
+        dec_unfolded_image, output_size=(28, 28), kernel_size=(8, 8), stride=3
     )
     print("dec_image.shape:", dec_image.shape, end="\n\n")
+    print(dec_image, end="\n\n")
