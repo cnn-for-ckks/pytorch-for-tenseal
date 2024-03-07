CIHUY: Fix backpropagation in convolutional layer

tests/MNIST/main.py
@@ -95,7 +95,7 @@ if __name__ == "__main__":
     original_model = ConvNet()
     original_model.load_state_dict(
         torch.load(
-            "./parameters/MNIST/trained-model.pth"
+            "./parameters/MNIST/original-model.pth"
         )
     )
 
@@ -122,17 +122,17 @@ if __name__ == "__main__":
     print()
 
     # Encrypted training
-    # enc_model = enc_train(
-    #     context,
-    #     enc_model,
-    #     enc_subset_train_loader,
-    #     enc_criterion,
-    #     enc_optimizer,
-    #     kernel_size=(4, 4),
-    #     stride=4,
-    #     n_epochs=10
-    # )
-    # print()
+    enc_model = enc_train(
+        context,
+        enc_model,
+        enc_subset_train_loader,
+        enc_criterion,
+        enc_optimizer,
+        kernel_size=(4, 4),  # BUG: Must not overlap
+        stride=4,  # BUG: Must not overlap
+        n_epochs=10
+    )
+    print()
 
     # Print the weights and biases of the model
     print("Ciphertext Model (After Training):")
@@ -145,6 +145,6 @@ if __name__ == "__main__":
         enc_model,
         enc_subset_test_loader,
         enc_criterion,
-        kernel_size=(4, 4),
-        stride=4,
+        kernel_size=(4, 4),  # BUG: Must not overlap
+        stride=4,  # BUG: Must not overlap
     )

tests/PyTorch/fold.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     x = torch.arange(N * C * H * W).view(N, C, H, W).float()
 
     # Parameters for unfold
-    # NOTE: Must not overlap
+    # BUG: Must not overlap
     kernel_size = (2, 2)
     stride = (2, 2)
     padding = (0, 0)  # Adding padding to see its effect

tests/TenSEAL/im2col_encoding.py
@@ -30,16 +30,16 @@ if __name__ == "__main__":
     image = torch.rand(1, 28, 28)
     print("image.shape:", image.shape, end="\n\n")
 
-    # NOTE: Plaintext im2col
+    # Plaintext im2col
     unfolded_image = unfold(
         image,
         kernel_size=(4, 4),
         stride=4,
         padding=0
-    )  # NOTE: Must not overlap
+    )  # BUG: Must not overlap
     print("unfolded_image.shape:", unfolded_image.shape, end="\n\n")
 
-    # NOTE: Plaintext convolution
+    # Plaintext convolution
     # Create the convolutional weight
     conv_weight = weight.view(1, -1)
 
@@ -51,14 +51,14 @@ if __name__ == "__main__":
     # print the result
     print("conv_output:", conv_output.tolist(), end="\n\n")
 
-    # NOTE: Encrypted im2col
+    # Encrypted im2col
     enc_unfolded_image = ts.enc_matmul_encoding(
         context, unfolded_image.t()
     )
     num_row = unfolded_image.shape[0]
     num_col = unfolded_image.shape[1]
 
-    # NOTE: Encrypted convolution
+    # Encrypted convolution
     # Create the convolutional weight
     enc_conv_weight = weight.view(-1)
 
@@ -70,7 +70,7 @@ if __name__ == "__main__":
     # Decrypt the result
     print("enc_result:", enc_result.decrypt(), end="\n\n")
 
-    # NOTE: Plaintext col2im
+    # Plaintext col2im
     raw_dec_unfolded_image = enc_unfolded_image.decrypt()
     dec_unfolded_image = torch.tensor(raw_dec_unfolded_image).reshape(
         len(raw_dec_unfolded_image) // num_col, num_col
@@ -87,7 +87,7 @@ if __name__ == "__main__":
         kernel_size=(4, 4),
         stride=4,
         padding=0
-    )  # NOTE: Must not overlap
+    )  # BUG: Must not overlap
     print("dec_image.shape:", dec_image.shape, end="\n\n")
 
     # Check if the original and folded tensors are equal