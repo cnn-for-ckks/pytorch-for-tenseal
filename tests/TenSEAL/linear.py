e3f82d1fc63c4c698ae16af160366e34c59cd557
rayhankinan <rayhankinan@gmail.com>
2024-04-05 21:04:50 +0700

    Add linear testing without batch

--- tests/TenSEAL/linear.py
+++ tests/TenSEAL/linear.py
@@ -24,26 +24,25 @@ if __name__ == "__main__":
     # Declare parameters
     out_features = 1
     in_features = 9
-    batch_size = 1
 
     # Create weight and input tensor
     weight = torch.randn(out_features, in_features)
-    input_tensor = torch.randn(batch_size, in_features)
+    input_tensor = torch.randn(in_features)
 
     # Create encrypted tensor
-    enc_input_tensor = ts.ckks_tensor(context, input_tensor.tolist())
+    enc_input_tensor = ts.ckks_vector(context, input_tensor.tolist())
 
     # Multiply the encrypted tensor with the weight tensor
     start_time = time.perf_counter()
-    enc_output = enc_input_tensor.mm(weight.t().tolist())
+    enc_output = enc_input_tensor.matmul(weight.t().tolist())
     end_time = time.perf_counter()
 
     # Decrypt the output tensor
-    dec_output = enc_output.decrypt().tolist()
+    dec_output = enc_output.decrypt()
     dec_output_tensor = torch.tensor(dec_output)
 
     # Compare the output with the target
-    target = input_tensor.mm(weight.t())
+    target = input_tensor.matmul(weight.t())
 
     # Check the correctness of the convolution via the toeplitz matrix
     enc_error = (dec_output_tensor - target).abs().sum()
@@ -51,8 +50,8 @@ if __name__ == "__main__":
     percent_of_enc_error = enc_error / sum_target * 100
 
     print(
-        f"Time taken for encrypted multiplication (with batch): {end_time - start_time:.6f} seconds"
+        f"Time taken for encrypted multiplication (without batch): {end_time - start_time:.6f} seconds"
     )
     print(
-        f"Percentage of encrypted convolution error (with batch): {percent_of_enc_error:.6f}%"
+        f"Percentage of encrypted convolution error (without batch): {percent_of_enc_error:.6f}%"
     )
