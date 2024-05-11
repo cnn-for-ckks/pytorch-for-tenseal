Fix multiple channel padding and multiple channel average convolution

src/torchseal/function/average2d.py
@@ -7,27 +7,32 @@ import torch
 
 class AvgPool2dFunction(torch.autograd.Function):
     @staticmethod
-    def forward(ctx: CKKSPoolingFunctionWrapper, enc_input: CKKSWrapper, weight: torch.Tensor, conv2d_input_mask: torch.Tensor) -> CKKSWrapper:
+    def forward(ctx: CKKSPoolingFunctionWrapper, enc_input: CKKSWrapper, weight: torch.Tensor, conv2d_padding_transformation: torch.Tensor, conv2d_inverse_padding_transformation: torch.Tensor) -> CKKSWrapper:
+        # Apply the padding transformation to the encrypted input
+        enc_padding_input = enc_input.do_matrix_multiplication(
+            conv2d_padding_transformation
+        )
+
         # Save the ctx for the backward method
-        ctx.save_for_backward(weight, conv2d_input_mask)
+        ctx.save_for_backward(weight, conv2d_inverse_padding_transformation)
 
         # Apply the convolution to the encrypted input
-        enc_output = enc_input.do_matrix_multiplication(weight.t())
+        enc_output = enc_padding_input.do_matrix_multiplication(weight.t())
 
         return enc_output
 
     # TODO: Move this to encrypted mode
     @staticmethod
-    def backward(ctx: CKKSPoolingFunctionWrapper, grad_output: torch.Tensor) -> Tuple[Optional[torch.Tensor], None, None]:
+    def backward(ctx: CKKSPoolingFunctionWrapper, grad_output: torch.Tensor) -> Tuple[Optional[torch.Tensor], None, None, None]:
         # Get the saved tensors
-        weight, conv2d_input_mask = typing.cast(
+        weight, conv2d_inverse_padding_transformation = typing.cast(
             Tuple[torch.Tensor, torch.Tensor],
             ctx.saved_tensors
         )
 
         # Get the needs_input_grad
         result = typing.cast(
-            Tuple[bool, bool, bool],
+            Tuple[bool, bool, bool, bool],
             ctx.needs_input_grad
         )
 
@@ -37,6 +42,8 @@ class AvgPool2dFunction(torch.autograd.Function):
         # Calculate the gradient for the input
         if result[0]:
             # Calculate the gradients for the input tensor (this will be encrypted)
-            grad_input = grad_output.matmul(weight).mul(conv2d_input_mask)
+            grad_input = grad_output.matmul(weight).matmul(
+                conv2d_inverse_padding_transformation
+            )
 
-        return grad_input, None, None
+        return grad_input, None, None, None

src/torchseal/function/conv2d.py
@@ -7,20 +7,25 @@ import torch
 
 class Conv2dFunction(torch.autograd.Function):
     @staticmethod
-    def forward(ctx: CKKSLinearFunctionWrapper, enc_input: CKKSWrapper, weight: torch.Tensor, bias: torch.Tensor, conv2d_input_mask: torch.Tensor, conv2d_weight_mask: torch.Tensor, conv2d_bias_transformation: torch.Tensor, training: bool) -> CKKSWrapper:
+    def forward(ctx: CKKSLinearFunctionWrapper, enc_input: CKKSWrapper, weight: torch.Tensor, bias: torch.Tensor, conv2d_padding_transformation: torch.Tensor, conv2d_inverse_padding_transformation: torch.Tensor, conv2d_weight_mask: torch.Tensor, conv2d_bias_transformation: torch.Tensor, training: bool) -> CKKSWrapper:
+        # Apply the padding transformation to the encrypted input
+        enc_padding_input = enc_input.do_matrix_multiplication(
+            conv2d_padding_transformation
+        )
+
         # Save the ctx for the backward method
         ctx.save_for_backward(
             weight,
-            conv2d_input_mask,
+            conv2d_inverse_padding_transformation,
             conv2d_weight_mask,
             conv2d_bias_transformation
         )
-        ctx.enc_input = enc_input.clone()
+        ctx.enc_input = enc_padding_input.clone()
         ctx.training = training
 
         # Apply the convolution to the encrypted input
         # TODO: Implement the convolution for encrypted parameters
-        enc_output = enc_input.do_matrix_multiplication(
+        enc_output = enc_padding_input.do_matrix_multiplication(
             weight.t()
         ).do_addition(bias)
 
@@ -30,7 +35,7 @@ class Conv2dFunction(torch.autograd.Function):
     @staticmethod
     def backward(ctx: CKKSLinearFunctionWrapper, grad_output: torch.Tensor) -> Tuple[Optional[torch.Tensor], Optional[torch.Tensor], Optional[torch.Tensor], None, None, None, None]:
         # Get the saved tensors
-        weight, conv2d_input_mask, conv2d_weight_mask, conv2d_bias_transformation = typing.cast(
+        weight, conv2d_inverse_padding_transformation, conv2d_weight_mask, conv2d_bias_transformation = typing.cast(
             Tuple[
                 torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor
             ],
@@ -49,7 +54,9 @@ class Conv2dFunction(torch.autograd.Function):
 
         if result[0]:
             # Calculate the gradients for the input tensor (this will be encrypted)
-            grad_input = grad_output.matmul(weight).mul(conv2d_input_mask)
+            grad_input = grad_output.matmul(weight).matmul(
+                conv2d_inverse_padding_transformation
+            )
 
         if result[1]:
             # Create the fully connected gradient weight tensor (this will be encrypted)

src/torchseal/nn/average2d.py
@@ -1,14 +1,14 @@
 from typing import Tuple
 from torchseal.wrapper import CKKSWrapper
 from torchseal.function import AvgPool2dFunction
-from torchseal.utils import approximate_toeplitz_multiple_channels, create_conv2d_input_mask
+from torchseal.utils import generate_near_zeros, approximate_toeplitz_multiple_channels, create_padding_transformation_matrix, create_inverse_padding_transformation_matrix
 
 import typing
 import torch
 
 
 class AvgPool2d(torch.nn.Module):
-    def __init__(self, n_channels: int, kernel_size: Tuple[int, int], input_size: Tuple[int, int], batch_size: int = 1, stride: int = 1, padding: int = 0,) -> None:
+    def __init__(self, n_channels: int, kernel_size: Tuple[int, int], input_size: Tuple[int, int], stride: int = 1, padding: int = 0) -> None:
         super(AvgPool2d, self).__init__()
 
         # Unpack the kernel size
@@ -17,30 +17,38 @@ class AvgPool2d(torch.nn.Module):
         # Unpack the input size
         input_height, input_width = input_size
 
-        # Create the weight
-        self.weight = approximate_toeplitz_multiple_channels(
-            torch.ones(
-                n_channels, n_channels, kernel_height, kernel_width
-            ).div(
+        # Create the initial weight
+        initial_weight = generate_near_zeros(
+            (n_channels, n_channels, kernel_height, kernel_width)
+        )
+
+        # Fill the initial weight with the average pooling kernel
+        for i in range(n_channels):
+            initial_weight[i, i] = torch.ones(kernel_height, kernel_width).div(
                 kernel_height * kernel_width
-            ),
+            )
+
+        self.weight = approximate_toeplitz_multiple_channels(
+            initial_weight,
             (n_channels, input_height, input_width),
             stride=stride,
             padding=padding
         )
 
-        # Create the binary masking for training
-        self.conv2d_input_mask = create_conv2d_input_mask(
-            (n_channels, input_height, input_width),
-            batch_size=batch_size,
-            padding=padding
+        # Create the binary masking for inference
+        self.conv2d_padding_transformation = create_padding_transformation_matrix(
+            n_channels, input_height, input_width, padding
+        )
+
+        self.conv2d_inverse_padding_transformation = create_inverse_padding_transformation_matrix(
+            n_channels, input_height, input_width, padding
         )
 
     def forward(self, enc_x: CKKSWrapper) -> CKKSWrapper:
         enc_output = typing.cast(
             CKKSWrapper,
             AvgPool2dFunction.apply(
-                enc_x, self.weight, self.conv2d_input_mask
+                enc_x, self.weight, self.conv2d_padding_transformation, self.conv2d_inverse_padding_transformation
             )
         )
 

src/torchseal/nn/conv2d.py
@@ -1,14 +1,14 @@
 from typing import Tuple, Optional
 from torchseal.wrapper import CKKSWrapper
 from torchseal.function import Conv2dFunction
-from torchseal.utils import approximate_toeplitz_multiple_channels, create_conv2d_input_mask, create_conv2d_weight_mask, create_conv2d_bias_transformation
+from torchseal.utils import approximate_toeplitz_multiple_channels, create_conv2d_weight_mask, create_conv2d_bias_transformation, create_padding_transformation_matrix, create_inverse_padding_transformation_matrix
 
 import typing
 import torch
 
 
 class Conv2d(torch.nn.Module):
-    def __init__(self, in_channels: int, out_channels: int, kernel_size: Tuple[int, int], input_size: Tuple[int, int], batch_size: int = 1, stride: int = 1, padding: int = 0, weight: Optional[torch.Tensor] = None, bias: Optional[torch.Tensor] = None) -> None:
+    def __init__(self, in_channels: int, out_channels: int, kernel_size: Tuple[int, int], input_size: Tuple[int, int], stride: int = 1, padding: int = 0, weight: Optional[torch.Tensor] = None, bias: Optional[torch.Tensor] = None) -> None:
         super(Conv2d, self).__init__()
 
         # Unpack the kernel size
@@ -44,13 +44,16 @@ class Conv2d(torch.nn.Module):
             ) if bias is None else bias
         )
 
-        # Create the binary masking for training
-        self.conv2d_input_mask = create_conv2d_input_mask(
-            (in_channels, input_height, input_width),
-            batch_size=batch_size,
-            padding=padding
+        # Create the binary masking for inference
+        self.conv2d_padding_transformation = create_padding_transformation_matrix(
+            in_channels, input_height, input_width, padding
         )
 
+        self.conv2d_inverse_padding_transformation = create_inverse_padding_transformation_matrix(
+            out_channels, input_height, input_width, padding
+        )
+
+        # Create the binary masking for training
         self.conv2d_weight_mask = create_conv2d_weight_mask(
             (in_channels, input_height, input_width),
             (out_channels, in_channels, kernel_height, kernel_width),
@@ -67,7 +70,7 @@ class Conv2d(torch.nn.Module):
         enc_output = typing.cast(
             CKKSWrapper,
             Conv2dFunction.apply(
-                enc_x, self.weight, self.bias, self.conv2d_input_mask, self.conv2d_weight_mask, self.conv2d_bias_transformation, self.training
+                enc_x, self.weight, self.bias, self.conv2d_padding_transformation, self.conv2d_inverse_padding_transformation, self.conv2d_weight_mask, self.conv2d_bias_transformation, self.training
             )
         )
 

src/torchseal/utils/padding.py
@@ -1,7 +1,25 @@
 import torch
 
 
-def create_inverse_padding_transformation_matrix(input_height: int, input_width: int, padding: int) -> torch.Tensor:
+def create_block_diagonal_matrix(base_matrix: torch.Tensor, num_blocks: int) -> torch.Tensor:
+    # Get dimensions of the base matrix
+    rows, cols = base_matrix.shape
+
+    # Create a large zero matrix to hold the entire block diagonal matrix
+    # NOTE: Possibility of TenSEAL parameter mismatch when using torch.zeros
+    block_diag_matrix = torch.zeros(num_blocks * rows, num_blocks * cols)
+
+    # Fill the block diagonal matrix
+    for i in range(num_blocks):
+        start_index = i * rows
+        block_diag_matrix[
+            start_index:start_index + rows, i * cols:i * cols + cols
+        ] = base_matrix
+
+    return block_diag_matrix
+
+
+def create_inverse_padding_transformation_matrix(num_channels: int, input_height: int, input_width: int, padding: int) -> torch.Tensor:
     # Add padding to the input size
     padded_input_height = input_height + 2 * padding
     padded_input_width = input_width + 2 * padding
@@ -24,12 +42,18 @@ def create_inverse_padding_transformation_matrix(input_height: int, input_width:
             # Update the inverse transformation matrix
             inverse_transform_matrix[orig_pos, padded_pos] = 1
 
-    transposed_inverse_transform_matrix = inverse_transform_matrix.t()
+    # Repeat the inverse transformation matrix for each channel
+    repeated_inverse_transform_matrix = create_block_diagonal_matrix(
+        inverse_transform_matrix, num_channels
+    )
+
+    # Transpose the inverse transformation matrix
+    transposed_inverse_transform_matrix = repeated_inverse_transform_matrix.t()
 
     return transposed_inverse_transform_matrix
 
 
-def create_padding_transformation_matrix(input_height: int, input_width: int, padding: int) -> torch.Tensor:
+def create_padding_transformation_matrix(num_channels: int, input_height: int, input_width: int, padding: int) -> torch.Tensor:
     # Add padding to the input size
     padded_input_height = input_height + 2 * padding
     padded_input_width = input_width + 2 * padding
@@ -52,6 +76,12 @@ def create_padding_transformation_matrix(input_height: int, input_width: int, pa
             # Update the transformation matrix
             transform_matrix[new_pos, orig_pos] = 1
 
-    transposed_transform_matrix = transform_matrix.t()
+    # Repeat the transformation matrix for each channel
+    repeated_transform_matrix = create_block_diagonal_matrix(
+        transform_matrix, num_channels
+    )
+
+    # Transpose the transformation matrix
+    transposed_transform_matrix = repeated_transform_matrix.t()
 
     return transposed_transform_matrix

tests/nn/test_average2d.py
@@ -33,20 +33,24 @@ def test_avgpool2d():
     context.generate_galois_keys()
 
     # Declare parameters
-    n_channels = 1
-    kernel_height = 4
-    kernel_width = 4
-    stride = 4
+    n_channels = 2
+    kernel_height = 3
+    kernel_width = 3
+    stride = 1
     padding = 1
 
     # Declare input dimensions
     batch_size = 1
-    input_height = 28
-    input_width = 28
+    input_height = 4
+    input_width = 4
+
+    # Adjust for padding
+    padded_input_height = input_height + 2 * padding
+    padded_input_width = input_width + 2 * padding
 
     # Count the output dimensions
-    output_height = (input_height - kernel_height) // stride + 1
-    output_width = (input_width - kernel_width) // stride + 1
+    output_height = (padded_input_height - kernel_height) // stride + 1
+    output_width = (padded_input_width - kernel_width) // stride + 1
 
     # Create the input tensor
     input_tensor = torch.randn(
@@ -55,9 +59,7 @@ def test_avgpool2d():
 
     # Encrypt the input tensor
     enc_input_tensor = torchseal.ckks_wrapper(
-        context, torch.nn.functional.pad(
-            input_tensor, (padding, padding, padding, padding)
-        ).view(batch_size, -1)
+        context, input_tensor.view(batch_size, -1)
     )
 
     # Create the plaintext average pooling layer

tests/torch/test_convmtx2.py
@@ -128,10 +128,10 @@ class ToeplitzConv2d(torch.nn.Module):
 
         # Create the binary masking for inference
         self.conv2d_padding_transformation = create_padding_transformation_matrix(
-            input_height, input_width, padding
+            in_channels, input_height, input_width, padding
         )
         self.conv2d_inverse_padding_transformation = create_inverse_padding_transformation_matrix(
-            input_height, input_width, padding
+            out_channels, input_height, input_width, padding
         )
 
         self.conv2d_weight_mask = create_conv2d_weight_mask(
@@ -165,16 +165,16 @@ def test_convmtx2():
 
     # Declare parameters
     out_channels = 2
-    in_channels = 1
-    kernel_height = 7
-    kernel_width = 7
-    stride = 3
+    in_channels = 2
+    kernel_height = 3
+    kernel_width = 3
+    stride = 1
     padding = 1
 
     # Declare input dimensions
     batch_size = 1
-    input_height = 28
-    input_width = 28
+    input_height = 4
+    input_width = 4
 
     # Adjust for padding
     padded_input_height = input_height + 2 * padding