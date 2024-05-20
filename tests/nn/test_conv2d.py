4fec7a1e87321107247a6604ef7f591f7294667a
rayhankinan <rayhankinan@gmail.com>
2024-05-20 20:47:28 +0700

    Add evaluation test for conv2d and linear layer

--- tests/nn/test_conv2d.py
+++ tests/nn/test_conv2d.py
@@ -211,6 +211,133 @@ def test_conv2d_train():
 
 
 def test_conv2d_eval():
-    # TODO: Add evaluation test
+    # Set the seed for reproducibility
+    torch.manual_seed(73)
+    np.random.seed(73)
+    random.seed(73)
 
-    pass
+    # Controls precision of the fractional part
+    bits_scale = 26
+
+    # Create TenSEAL context
+    context = ts.context(
+        ts.SCHEME_TYPE.CKKS,
+        poly_modulus_degree=8192,
+        coeff_mod_bit_sizes=[
+            31, bits_scale, bits_scale, bits_scale, bits_scale, bits_scale, bits_scale, 31
+        ]
+    )
+
+    # Set the scale
+    context.global_scale = pow(2, bits_scale)
+
+    # Galois keys are required to do ciphertext rotations
+    context.generate_galois_keys()
+
+    # Set the context
+    torchseal.set_context(context)
+
+    # Declare parameters
+    # NOTE: High number of padding will cause the test to fail (due to the added noise by generating near-zero values)
+    out_channels = 2
+    in_channels = 2
+    kernel_height = 3
+    kernel_width = 3
+    stride = 1
+    padding = 0
+
+    # Declare input dimensions
+    batch_size = 1
+    input_height = 4
+    input_width = 4
+
+    # Adjust for padding
+    padded_input_height = input_height + 2 * padding
+    padded_input_width = input_width + 2 * padding
+
+    # Count the output dimensions
+    output_height = (padded_input_height - kernel_height) // stride + 1
+    output_width = (padded_input_width - kernel_width) // stride + 1
+
+    # Create weight and bias
+    kernel = torch.randn(
+        out_channels,
+        in_channels,
+        kernel_height,
+        kernel_width,
+        requires_grad=True
+    )
+    bias = torch.randn(out_channels, requires_grad=True)
+
+    # Create the input tensor
+    input_tensor = torch.randn(
+        batch_size, in_channels, input_height, input_width, requires_grad=True
+    )
+
+    # Encrypt the input tensor
+    enc_input_tensor = torchseal.ckks_wrapper(
+        input_tensor.view(batch_size, -1), do_encryption=True
+    )
+    enc_input_tensor.requires_grad_(True)
+
+    # Create the plaintext convolution layer
+    conv2d = PlainConv2d(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=(kernel_height, kernel_width),
+        stride=stride,
+        padding=padding,
+        bias=True,
+    )
+    conv2d.weight = torch.nn.Parameter(kernel)  # Set the weight
+    conv2d.bias = torch.nn.Parameter(bias)  # Set the bias
+
+    # Create the encrypted convolution layer
+    enc_conv2d = EncryptedConv2d(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=(kernel_height, kernel_width),
+        input_size=(input_height, input_width),
+        stride=stride,
+        padding=padding,
+        weight=torchseal.ckks_wrapper(
+            approximate_toeplitz_multiple_channels(
+                kernel,
+                (in_channels, input_height, input_width),
+                stride=stride,
+                padding=padding
+            ),
+            do_encryption=True
+        ),
+        bias=torchseal.ckks_wrapper(
+            torch.repeat_interleave(
+                bias,
+                output_height * output_width
+            ),
+            do_encryption=True
+        ),
+    )
+
+    # Set both layer on eval mode
+    conv2d.eval()
+    enc_conv2d.eval()
+
+    # Calculate the output
+    output = conv2d.forward(input_tensor)
+    enc_output = enc_conv2d.forward(enc_input_tensor)
+
+    # Decrypt the output
+    dec_output = enc_output.decrypt().plaintext_data.clone()
+
+    # Resized the output
+    output_resized = output.view(
+        batch_size, -1
+    )
+
+    # Check the correctness of the convolution (with a tolerance of 5e-2)
+    assert torch.allclose(
+        dec_output,
+        output_resized,
+        atol=5e-2,
+        rtol=0
+    ), "Convolution layer failed!"
--- tests/nn/test_linear.py
+++ tests/nn/test_linear.py
@@ -149,6 +149,81 @@ def test_linear_train():
 
 
 def test_linear_eval():
-    # TODO: Add evaluation test
+    # Set the seed for reproducibility
+    torch.manual_seed(73)
+    np.random.seed(73)
+    random.seed(73)
 
-    pass
+    # Controls precision of the fractional part
+    bits_scale = 26
+
+    # Create TenSEAL context
+    context = ts.context(
+        ts.SCHEME_TYPE.CKKS,
+        poly_modulus_degree=8192,
+        coeff_mod_bit_sizes=[
+            31, bits_scale, bits_scale, bits_scale, bits_scale, bits_scale, bits_scale, 31
+        ]
+    )
+
+    # Set the scale
+    context.global_scale = pow(2, bits_scale)
+
+    # Galois keys are required to do ciphertext rotations
+    context.generate_galois_keys()
+
+    # Set the context
+    torchseal.set_context(context)
+
+    # Declare parameters
+    out_features = 8
+    in_features = 8
+
+    # Declare input dimensions
+    batch_size = 1
+
+    # Create weight and bias
+    weight = torch.randn(out_features, in_features, requires_grad=True)
+    bias = torch.randn(out_features, requires_grad=True)
+
+    # Create the input tensor
+    input_tensor = torch.randn(batch_size, in_features, requires_grad=True)
+
+    # Encrypt the input tensor
+    enc_input_tensor = torchseal.ckks_wrapper(
+        input_tensor.clone().view(batch_size, -1), do_encryption=True
+    )
+    enc_input_tensor.requires_grad_(True)
+
+    # Create the plaintext linear layer
+    linear = PlainLinear(in_features, out_features)
+    linear.weight = torch.nn.Parameter(weight)
+    linear.bias = torch.nn.Parameter(bias)
+
+    # Create the encrypted linear layer
+    enc_linear = EncryptedLinear(
+        in_features=in_features,
+        out_features=out_features,
+        weight=torchseal.ckks_wrapper(
+            weight.clone(), do_encryption=True
+        ),
+        bias=torchseal.ckks_wrapper(
+            bias.clone(), do_encryption=True
+        )
+    )
+
+    # Set both layer on eval mode
+    linear.eval()
+    enc_linear.eval()
+
+    # Calculate the output
+    output = linear.forward(input_tensor)
+    enc_output = enc_linear.forward(enc_input_tensor)
+
+    # Decrypt the output
+    dec_output = enc_output.decrypt().plaintext_data.clone()
+
+    # Check the correctness of the convolution (with a tolerance of 5e-2)
+    assert torch.allclose(
+        output, dec_output, atol=5e-2, rtol=0
+    ), "Linear layer failed!"
